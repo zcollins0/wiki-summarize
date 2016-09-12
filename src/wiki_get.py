6d5a72c6b3886251db00d03bf9993236221c0457
Laura Tebben <tebbenla@mail.uc.edu>
2016-09-11 23:16:36 -0400

    added comments

--- src/wiki_get.py
+++ src/wiki_get.py
@@ -4,6 +4,7 @@ def summarize(topic):
 	try:
 		print(wikipedia.summary(topic))
 		return[]
+	#catch exceptions so users don't see block of error messages	
 	except (IndexError, wikipedia.PageError):
 			print("Invalid page. Searching...")
 			t = wikipedia.search(topic)
