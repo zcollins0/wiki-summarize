8fb27a332e82528e9e7241f750187daf15b27068
Zach Collins <collinzy@mail.uc.edu>
2016-09-16 19:48:27 -0400

    Add more exceptions

--- src/wiki_get.py
+++ src/wiki_get.py
@@ -3,12 +3,13 @@ import wikipedia
 def summarize(topic):
 	try:
 		print(wikipedia.summary(topic))
-		return[]
+		return None
 	#catch exceptions so users don't see block of error messages	
-	except (IndexError, wikipedia.PageError):
+	except (IndexError, wikipedia.PageErrori, wikipedia.DisambiguationError):
 			print("Invalid page. Searching...")
 			t = wikipedia.search(topic)
 			if(t == []):
 				print("No results match your search.")
 			return(t)
+
 
